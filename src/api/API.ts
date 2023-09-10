e181e03d32ea9c5fe757595563124d9137be6955
Rafael <rafael.soufer@endava.com>
2023-09-10 18:03:02 -0300

    added important comment

--- src/api/API.ts
+++ src/api/API.ts
@@ -4,6 +4,7 @@ export const BASE_URL = 'https://api.themoviedb.org/3/';
 export const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
 
 export const API = axios.create({
+  //NOTE: This token should be get on a safer way and not hardocoded on FE code, this is here for demosntration purposes only
   headers: {
     Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MmVlN2M2ZTJiMmU2M2U0ZDljYzk0NTg0Nzg2Yjg0NCIsInN1YiI6IjYwYTNjNDAyZTI3MjYwMDA1OGI3NjE2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8x6dXQC2ua5DW8AawnPaUjC80h857lVI7yixv0gPL-A`,
   },
